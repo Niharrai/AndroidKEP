2f17181e540dbc03625bbed39c92b51e40cf29a2
aniruddhsujish <aniruddhsujish1113@gmail.com>
2020-10-18 01:39:16 +0530

    RecyclerView Added

---
+++ KEP_AS/app/src/main/java/com/example/kep_as/DataClass.java
@@ -0,0 +1,13 @@
+package com.example.kep_as;
+
+public class DataClass {
+    String name;
+    String mob;
+    int img_name;
+
+    DataClass(String mName, String mMob, int mImg_name){
+        name = mName;
+        mob = mMob;
+        img_name = mImg_name;
+    }
+}
